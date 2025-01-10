c067fbbf53e15db758cc31fb7bed56cf825a7798
IL <isakl.revinge@gmail.com>
2025-01-10 16:33:33 +0100

    Tools: Added State

--- tools.py
+++ tools.py
@@ -1,6 +1,15 @@
 import threading
 from functools import wraps
 
+class State:
+    """Controls the state of the program."""
+
+    running = True
+    paused = False
+    
+    def __init__(self) -> None:
+        raise RuntimeError("State class is not meant to be initialized")
+
 def threaded(func):
 
     @wraps(func)
