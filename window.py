91f7b77b606d6e72c4b2e207aba88e3dc757ebf1
PlingForsman <isakl.revinge@gmail.com>
2025-01-10 11:00:03 +0100

    Window: Variable rename

--- window.py
+++ window.py
@@ -8,7 +8,7 @@ class ProcessWindow:
         ctypes.windll.user32.SetProcessDPIAware()
 
         self.hwnd = self.find_window("UnrealWindow", "ArkAscended")
-        self.resolution = self.get_window_resolution()
+        self.resolution = self.get_resolution()
         self.template_path = f"{Path(__file__).parent}/templates"
         self.ocr_path = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
 
@@ -30,7 +30,7 @@ class ProcessWindow:
         else: 
             return hwnd
 
-    def get_window_resolution(self) -> tuple[int, int]:
+    def get_resolution(self) -> tuple[int, int]:
 
         left, top, right, bot = win32gui.GetWindowRect(self.hwnd)
 
